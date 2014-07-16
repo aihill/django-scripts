3131f5d9ff80e423bda3b3a535dcd9cc30752493
Sean Bell <sbell@cs.cornell.edu>
2014-07-16 14:51:30 -0400

    bugfix

--- celery_worker.sh
+++ celery_worker.sh
@@ -11,19 +11,20 @@ if [ $# -ge 1 ]; then
 	concurrency=$1
 fi
 
-user=$SERVER_USER
+queue=celery
 if [ $# -ge 2 ]; then
-	user=$2
+	queue=$2
 fi
 
+user=$SERVER_USER
+
 # TODO: run as a background process with a higher log level
-celery_cmd="source $VENV_DIR/bin/activate; builtin cd $SRC_DIR; celery worker -B -A config -Q celery --loglevel=info --concurrency=$concurrency"
+celery_cmd="source $VENV_DIR/bin/activate; builtin cd $SRC_DIR; $VENV_DIR/bin/celery worker -B -A $PROJECT_NAME -Q $queue --loglevel=info --concurrency=$concurrency"
 
 set -x
+sudo rm -f $SRC_DIR/celerybeat-schedule
 if [[ $USER == $user ]]; then
-	rm -f $SRC_DIR/celerybeat-schedule
 	"$celery_cmd"
 else
-	sudo rm -f $SRC_DIR/celerybeat-schedule
 	sudo -u $user bash -c "$celery_cmd"
 fi
