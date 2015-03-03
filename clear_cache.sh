be76e52c42e7a5e00aa2834d448c90f096000dd1
Sean Bell <sbell@cs.cornell.edu>
2015-03-03 16:49:23 -0500

    added clear_cache script

---
+++ clear_cache.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# This flushes the site-wide cache
+#
+
+DIR="$( builtin cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" && pwd )"
+source $DIR/load_config.sh
+
+$VENV/bin/python $SRC_DIR/manage.py clear_cache
